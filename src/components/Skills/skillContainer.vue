55318b64be67bd3b8ee46bba39b671817afa515b
Peter Iskander <petermagdyiskander@gmail.com>
2023-08-04 11:14:03 +0300

    inactive skills

--- src/components/Skills/skillContainer.vue
+++ src/components/Skills/skillContainer.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="skill-container" @click="dealPoints">
+    <div class="skill-container" @click="dealPoints" :class="{ 'inactive': !active }">
         <h6>{{ skillName }}</h6>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 export default {
     name: "skill-container",
     props: {
@@ -26,17 +27,18 @@ export default {
     },
     methods: {
         dealPoints() {
-            if(this.active){
-                this.$emit('dealPoints',this.skillPoints)
+            if (this.active) {
+                this.$emit('dealPoints', this.skillPoints)
             }
         }
-    }
+    },
+    computed: mapGetters(['getUser']),
 }
 </script>
 <style lang="scss" scoped>
 .skill-container {
     padding: 12px 8px;
-    cursor:pointer;
+    cursor: pointer;
     background-color: #3E8898;
     border: 2px solid #FFFFFF;
     box-shadow: 0px 0px 6px #0000003B;
@@ -44,7 +46,8 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    h6{
+
+    h6 {
         padding: 0;
         margin: 0;
         color: #E5E5E5;
@@ -52,5 +55,8 @@ export default {
         font-family: 'pressstart2p';
     }
 
+}
+.inactive{
+    opacity:0.5;
 }
 </style>
