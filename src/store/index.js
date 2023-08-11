963b4fefa63023808d329c98c6a7db6b114298e9
Peter Iskander <petermagdyiskander@gmail.com>
2023-08-11 14:30:26 +0300

    fixes

--- src/store/index.js
+++ src/store/index.js
@@ -30,31 +30,306 @@ export default createStore({
           allUsers = snapshot.docs.map(doc => doc.id);
           if (!allUsers.includes(res.user.uid)) {
             newUser = true;
+            let userDB = {
+              "evan20012@gmail.com": {
+                role: "Attacker",
+                team: "Dynamis"
+              },
+              "samueladel960@gmail.com": {
+                role: "Attacker",
+                team: "Dynamis"
+              },
+              "andrewhany718@gmail.com": {
+                role: "Attacker",
+                team: "Dynamis"
+              },
+              "martinmaged72@gmail.com": {
+                role: "Attacker",
+                team: "Kalos"
+              },
+              "claratadros@gmail.com": {
+                role: "Healer",
+                team: "Kalos"
+              },
+              "miret.mm11@gmail.com": {
+                role: "Defender",
+                team: "Dynamis"
+              },
+              "rafikghazarian249@gmail.com": {
+                role: "Defender",
+                team: "Kalos"
+              },
+              "youstinanabil78@gmail.com": {
+                role: "Defender",
+                team: "Lumos"
+              },
+              "sandywael640@gmail.com": {
+                role: "Defender",
+                team: "Dynamis"
+              },
+              "emymaged595@gmail.com": {
+                role: "Defender",
+                team: "Kalos"
+              },
+              "sossana.moneeb.sh@gmail.com": {
+                role: "attacker",
+                team: "Kalos"
+              },
+              "kevinwaeil21@gmail.com": {
+                role: "Attacker",
+                team: "Lumos"
+              },
+              "lodymarcos2005@gmail.com": {
+                role: "Attacker",
+                team: "Lumos"
+              },
+              "imagehany@gmail.com": {
+                role: "Defender",
+                team: "Dynamis"
+              },
+              "logymaher10@gmail.com": {
+                role: "Healer",
+                team: "Lumos"
+              },
+              "magdyjoseph09@gmail.com": {
+                role: "Healer",
+                team: "Lumos"
+              },
+              "marymagdykamal@gmail.com": {
+                role: "Healer",
+                team: "Dynamis"
+              },
+              "tonyakmalstudy111@gmail.com": {
+                role: "Defender",
+                team: "Lumos"
+              },
+              "martintarek211@gmail.com": {
+                role: "Attacker",
+                team: "Lumos"
+              },
+              "cloudiae9@gmail.com": {
+                role: "Defender",
+                team: "Lumos"
+              },
+              "farahwahid79@gmail.com": {
+                role: "Defender",
+                team: "Lumos"
+              },
+              "dodzmagdykamal@gmail.com": {
+                role: "Healer",
+                team: "Lumos"
+              },
+              "balymagdy11@gmail.com": {
+                role: "Attacker",
+                team: "Lumos"
+              },
+              "monicaemil39@gmail.com": {
+                role: "Defender",
+                team: "Lumos"
+              },
+              "sarahsherif2002@gmail.com": {
+                role: "Defender",
+                team: "Lumos"
+              },
+              "karimsameh003@gmail.com": {
+                role: "Healer",
+                team: "Kalos"
+              },
+              "nardeen.makhlouf@gmail.com": {
+                role: "Defender",
+                team: "Dynamis"
+              },
+              "karenbassem91@gmail.com": {
+                role: "Defender",
+                team: "Dynamis"
+              },
+              "mitiray12@gmail.com": {
+                role: "Healer",
+                team: "Astro"
+              },
+              "patrickk.nashed@gmail.com": {
+                role: "Attacker",
+                team: "Astro"
+              },
+              "jessysaber6@gmail.com": {
+                role: "Healer",
+                team: "Astro"
+              },
+              "johnmalak133@gmail.com": {
+                role: "Attacker",
+                team: "Astro"
+              },
+              "bimennagy16@gmail.com": {
+                role: "Healer",
+                team: "Kalos"
+              },
+              "ramymourad23@gmail.com": {
+                role: "attacker",
+                team: "Kalos"
+              },
+              "merolaboles4@gmail.com": {
+                role: "Defender",
+                team: "Astro"
+              },
+              "koko.george456@gmail.com": {
+                role: "Attacker",
+                team: "Astro"
+              },
+              "samehraafat1231@gmail.com": {
+                role: "Attacker",
+                team: "Astro"
+              },
+              "lanaemil1999@gmail.com": {
+                role: "Healer",
+                team: "Astro"
+              },
+              "gloryraafat@gmail.com": {
+                role: "Defender",
+                team: "Astro"
+              },
+              "joyhelmy.tla@gmail.com": {
+                role: "Healer",
+                team: "Lumos"
+              },
+              "maykelgorge315@gmail.com": {
+                role: "Defender",
+                team: "Kalos"
+              },
+              "mariemagdi6@gmail.com": {
+                role: "Attacker",
+                team: "Astro"
+              },
+              "jyoutham@gmail.com": {
+                role: "Attacker",
+                team: "Lumos"
+              },
+              "georgeyoutham@gmail.com": {
+                role: "Attacker",
+                team: "Astro"
+              },
+              "assermedhat59@gmail.com": {
+                role: "Defender",
+                team: "Lumos"
+              },
+              "amymageda@gmail.com": {
+                role: "Healer",
+                team: "Astro"
+              },
+              "sandyjoseph6006@gmail.com": {
+                role: "Defender",
+                team: "Astro"
+              },
+              "anne.hanyy@gmail.com": {
+                role: "Defender",
+                team: "Astro"
+              },
+              "markdoha2017@gmail.com": {
+                role: "Attacker",
+                team: "Astro"
+              },
+              "davidmhamdi@gmail.com": {
+                role: "Healer",
+                team: "Dynamis"
+              },
+              "fadysamehwilliam@gmail.com": {
+                role: "Attacker",
+                team: "Dynamis"
+              },
+              "oliviam8080@gmail.com": {
+                role: "Defender",
+                team: "Astro"
+              },
+              "jona123669@gmail.com": {
+                role: "Attacker",
+                team: "Dynamis"
+              },
+              "joellenachaat@gmail.com": {
+                role: "Defender",
+                team: "Dynamis"
+              },
+              "demyan.helmy132@gmail.com": {
+                role: "Healer",
+                team: "Dynamis"
+              },
+              "juliehelmy2211@gmail.com": {
+                role: "Healer",
+                team: "Dynamis"
+              },
+              "nardinewageh@gmail.com": {
+                role: "Defender",
+                team: "Dynamis"
+              },
+              "fadixbishara@gmail.com": {
+                role: "Defender",
+                team: "Astro"
+              },
+              "christyadeln@gmail.com": {
+                role: "Attacker",
+                team: "Kalos"
+              },
+              "dannyjv24@gmail.com": {
+                role: "Attacker",
+                team: "Kalos"
+              },
+              "carolk.nashed@gmail.com": {
+                role: "Healer",
+                team: "Lumos"
+              },
+              "youssmoury@gmail.com": {
+                role: "Defender",
+                team: "Kalos"
+              },
+              "johnkokiraafat@gmail.com": {
+                role: "Attacker",
+                team: "Kalos"
+              },
+              "mariamelhenawy849@gmail.com": {
+                role: "Defender",
+                team: "Kalos"
+              },
+              "erenyhenawy55@gmail.com": {
+                role: "Defender",
+                team: "Kalos"
+              },
+              "davidwagih650@gmail.com": {
+                role: "Attacker",
+                team: "Lumos"
+              },
+              "miratef71@gmail.com": {
+                role: "Healer",
+                team: "Astro"
+              },
+              "petermagdyiskander@gmail.com": {
+                role: "Attacker",
+                team: "Dynamis"
+              }
+            }
+            console.log(res.user.email)
             setDoc(doc(firestore, "users", res.user.uid), {
               assignedQuestId: "",
-              attackAmp:1,
-              blockAmp:1,
-              healAmp:1,
+              attackAmp: 1,
+              blockAmp: 1,
+              healAmp: 1,
               exp: 0,
               gold: 0,
               level: 0,
               name: res.user.displayName,
-              role: "HEALER",
-              teamId: "Dynamis",
+              role: userDB[res.user.email].role.toUpperCase(),
+              teamId: userDB[res.user.email].team,
               timeStamp: "2023-08-09T18:05:35.146Z",
             }, { merge: true });
             let user = {
               uid: res.user.uid,
               assignedQuestId: "",
-              attackAmp:1,
-              blockAmp:1,
-              healAmp:1,
+              attackAmp: 1,
+              blockAmp: 1,
+              healAmp: 1,
               exp: 0,
               gold: 0,
               level: 0,
               name: res.user.displayName,
-              role: "HEALER",
-              teamId: "Dynamis",
+              role: userDB[res.user.email].role.toUpperCase(),
+              teamId: userDB[res.user.email].team,
               timeStamp: "2023-08-09T18:05:35.146Z",
             }
             commit('setUser', user)
@@ -148,12 +423,12 @@ export default createStore({
                 healAmp: 0.5,
                 blockAmp: 0.5
               },
-              14:{
+              14: {
                 neededExp: 35000000,
                 attackAmp: 0.5,
                 healAmp: 0.5,
                 blockAmp: 0.5
-            }
+              }
             }
 
             const data = snapshot.data();
